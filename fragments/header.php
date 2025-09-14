e2984dee54371ce18ee16e224e27ee5863096600
Jorge Flores <jorgeafa.19@gmail.com>
2025-09-13 21:29:12 -0500

    Se agregó el header

---
+++ fragments/header.php
@@ -0,0 +1,46 @@
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img src="imagenes/utp.png" alt="Logo" class="img-fluid rounded-circle" 
+                     style="max-width: 32px"/>
+                Proyecto Final
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            INV1 - Método de Busqueda
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="inv1/binario/intranet1.php">Búsqueda Binaria</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            INV2 - Listas Enlazadas
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="inv2/listas_simples/intranet2.php">Simples</a></li>
+                            <li><a class="dropdown-item" href="inv2/listas_dobles/intranet2.php">Dobles</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            INV3 - Árboles
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="inv3/arbol_binario/intranet3.php">Árboles Binarios</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
